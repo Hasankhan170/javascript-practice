dfa05c45e9321ee3d9171a9c2080f70df2495a16
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-24 23:48:17 -0700

    practice

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Ternary Operator
 
 // const age = 20;
@@ -13,58 +14,52 @@
 // console.log(arrow('hasan'));
 
 
-const small = document.querySelector('.small')
-let users = [];
+// const small = document.querySelector('.small')
+// let users = [];
 
-async function asyncFun(){
-try {
-    const res = await axios('https://jsonplaceholder.typicode.com/users');
-    users = res.data;
-    randarItems();
-} catch (error) {
-    console.log(error);
-}
-}
+// async function asyncFun(){
+// try {
+//     const res = await axios('https://jsonplaceholder.typicode.com/users');
+//     users = res.data;
+//     randarItems();
+// } catch (error) {
+//     console.log(error);
+// }
+// }
 
-function randarItems(){
-    small.innerHTML = users.map((item,index)=>`
-     <div class="card" style="width: 18rem;" data-index="${index}">
-                    <div class="card-body">
-                        <h5 class="card-title">${item.name}</h5>
-                        <p class="card-text">Address: ${item.address.suite}, ${item.address.street}, ${item.address.city}</p>
-                    </div>
-                </div>
-    `).join('');
+// function randarItems(){
+//     small.innerHTML = users.map((item,index)=>`
+//      <div class="card" style="width: 18rem;" data-index="${index}">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${item.name}</h5>
+//                         <p class="card-text">Address: ${item.address.suite}, ${item.address.street}, ${item.address.city}</p>
+//                     </div>
+//                 </div>
+//     `).join('');
 
-    document.querySelectorAll('.card').forEach(card=>
-        card.addEventListener('click', deleteCard)
-    )
-}
-
-
-document.querySelectorAll('.card').forEach(card=>
-    card.addEventListener('click', deleteCard)
-)
-
-function deleteCard(event){
-    const card = event.target.closest('.card');
-    const index = card.getAttribute('data-index');
-    users.splice(index,1)
-    randarItems();
-}
+//     document.querySelectorAll('.card').forEach(card=>
+//         card.addEventListener('click', deleteCard)
+//     )
+// }
 
 
+// document.querySelectorAll('.card').forEach(card=>
+//     card.addEventListener('click', deleteCard)
+// )
 
-asyncFun();
+// function deleteCard(event){
+//     const card = event.target.closest('.card');
+//     const index = card.getAttribute('data-index');
+//     users.splice(index,1)
+//     randarItems();
+// }
 
 
 
+// asyncFun();
 
 
-
-
-
-
+// async await 
 
 
 // async function asyncFun(){
@@ -92,3 +87,25 @@ asyncFun();
 // }
 
 // asyncFun();
+
+
+const div = document.querySelector('div')
+const obj = {
+    name: 'hasan',
+    age: 20,
+    isloggedIn : true,
+}
+
+// let value = Object.values(obj)
+// for(let i = 0; i < value.length;i++){
+//     console.log(value[i]);
+//     div.innerHTML += `<h1>${value[i]}</h1>`
+// }
+
+
+
+// Object.entries(obj).forEach((i)=>{
+//    console.log(i);
+
+    
+// });
